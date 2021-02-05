fcd95b9516e8cc870a353b19622c6877e226945b
FmFromB <63696790+FmFromB@users.noreply.github.com>
2021-02-05 18:13:02 +1000

    Add files via upload

--- stormwind_app/models.py
+++ stormwind_app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
 class Property(models.Model):
     PROPERTY_TYPES = [
         ('Дом','Дом'),
@@ -18,7 +16,7 @@ class Property(models.Model):
     rooms = models.IntegerField('Количество комнат', null=True, blank=True)
     area = models.IntegerField('Площадь')
     floor_count = models.IntegerField('Количество этажей', null=True, blank=True)
-    property_type = models.CharField('Тип собственности', choices=PROPERTY_TYPES, max_length=20, null=True, blank=True)
+    property_type = models.CharField('Тип собственности', choices=PROPERTY_TYPES, max_length=20, null=True)
 
     class Meta:
         verbose_name='запрос'
@@ -33,16 +31,16 @@ class Client(models.Model):
     last_name = models.CharField('Фамилия', max_length=100, null=True, blank=True)
     middle_name = models.CharField('Отчество', max_length=100, null=True, blank=True)
     phone = models.IntegerField('Телефон', blank=True, null=True)
-    email = models.CharField('Email', max_length=100, null=True, blank=True)
+    email = models.CharField('Email', max_length=100, null=True)
 
     def __str__(self):
         return str(self.id)
 
 class Realtor(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто добавил', null=True, blank=True)
-    first_name = models.CharField('Имя', max_length=100, null=True, blank=True)
-    last_name = models.CharField('Фамилия', max_length=100, null=True, blank=True)
-    middle_name = models.CharField('Отчество', max_length=100, null=True, blank=True)
+    first_name = models.CharField('Имя', max_length=100, null=True)
+    last_name = models.CharField('Фамилия', max_length=100, null=True)
+    middle_name = models.CharField('Отчество', max_length=100, null=True)
     commission = models.IntegerField('Комиссия', blank=True, null=True)
 
     def __str__(self):
@@ -51,12 +49,11 @@ class Realtor(models.Model):
 
 class Offer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор обьявления', null=True, blank=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Номер клиента', null=True, blank=True)
-    realtor = models.ForeignKey(Realtor, on_delete=models.CASCADE, verbose_name='Номер риэлтора', null=True, blank=True)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, verbose_name='Номер недвижимости', null=True, blank=True)
-    price = models.IntegerField('Цена', blank=True, null=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Номер клиента', null=True)
+    realtor = models.ForeignKey(Realtor, on_delete=models.CASCADE, verbose_name='Номер риэлтора', null=True)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, verbose_name='Номер недвижимости', null=True)
+    price = models.IntegerField('Цена', null=True)
     price_percent = models.IntegerField('Цена c процентами', blank=True, null=True)
-    address = models.IntegerField('[ppfxtv', blank=True, null=True)
 
     class Meta:
         verbose_name='предложение'
@@ -72,9 +69,9 @@ class Req(models.Model):
         ('Земля', 'Земля')
         ]
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор обьявления', null=True, blank=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Номер клиента', null=True, blank=True)
-    realtor = models.ForeignKey(Realtor, on_delete=models.CASCADE, verbose_name='Номер риэлтора', null=True, blank=True)
-    property_type = models.CharField('Тип имущества', choices=PROPERTY_TYPES, max_length=100, blank=True, null=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Номер клиента', null=True)
+    realtor = models.ForeignKey(Realtor, on_delete=models.CASCADE, verbose_name='Номер риэлтора', null=True)
+    property_type = models.CharField('Тип имущества', choices=PROPERTY_TYPES, max_length=100)
     min_price = models.IntegerField('Цена от', blank=True, null=True)
     max_price = models.IntegerField('До', blank=True, null=True)
     min_area = models.IntegerField('Минимальная площадь (кв. метров)', blank=True, null=True)
@@ -90,9 +87,9 @@ class Req(models.Model):
         return str(self.id)
 
 class Deal(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор сделки', null=True, blank=True)
-    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, verbose_name='Номер предложения', null=True, blank=True)
-    req = models.ForeignKey(Req, on_delete=models.CASCADE, verbose_name='Номер запроса', null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор сделки', null=True)
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, verbose_name='Номер предложения', null=True)
+    req = models.ForeignKey(Req, on_delete=models.CASCADE, verbose_name='Номер запроса', null=True)
 
 
 
